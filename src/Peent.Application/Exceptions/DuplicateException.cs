6e5131e2b679d372da6fb6422aafd30c62cb30d8
Robert Gonek <rgonek@gmail.com>
2019-09-01 17:45:06 +0200

    Easier creation of not found and duplication exceptions

--- src/Peent.Application/Exceptions/DuplicateException.cs
+++ src/Peent.Application/Exceptions/DuplicateException.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Peent.Application.Exceptions
 {
     public class DuplicateException : Exception
     {
         public string EntityName { get; set; }
+        public string KeyName { get; set; }
         public object Key { get; set; }
 
-        public override string Message => $"Entity \"{EntityName}\" ({Key}) already exists.";
+        public override string Message =>
+            string.IsNullOrEmpty(KeyName) ?
+                $"Entity \"{EntityName}\" ({Key}) already exists." :
+                $"Entity \"{EntityName}\" ({KeyName}: {Key}) already exists.";
 
         public DuplicateException(string name, object key)
         {
             EntityName = name;
             Key = key;
+        }
+
+        public DuplicateException(string name, string keyName, object key)
+        {
+            EntityName = name;
+            KeyName = keyName;
+            Key = key;
+        }
+
+        public static DuplicateException Create<TEntity>(object key)
+            where TEntity : class
+        {
+            return new DuplicateException(typeof(TEntity).Name, key);
+        }
+
+        public static DuplicateException Create<TEntity>(Expression<Func<TEntity, object>> expression, object key)
+            where TEntity : class
+        {
+            var member = (MemberExpression)expression.Body;
+            return new DuplicateException(typeof(TEntity).Name, member.Member.Name, key);
         }
     }
 }
--- src/Peent.Application/Exceptions/NotFoundException.cs
+++ src/Peent.Application/Exceptions/NotFoundException.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Peent.Application.Exceptions
 {
     public class NotFoundException : Exception
     {
         public string EntityName { get; set; }
+        public string KeyName { get; set; }
         public object Key { get; set; }
 
-        public override string Message => $"Entity \"{EntityName}\" ({Key}) was not found.";
+        public override string Message =>
+            string.IsNullOrEmpty(KeyName) ?
+                $"Entity \"{EntityName}\" ({Key}) already exists." :
+                $"Entity \"{EntityName}\" ({KeyName}: {Key}) already exists.";
 
         public NotFoundException(string name, object key)
         {
             EntityName = name;
             Key = key;
+        }
+
+        public NotFoundException(string name, string keyName, object key)
+        {
+            EntityName = name;
+            KeyName = keyName;
+            Key = key;
+        }
+
+        public static NotFoundException Create<TEntity>(object key)
+            where TEntity : class
+        {
+            return new NotFoundException(typeof(TEntity).Name, key);
+        }
+
+        public static NotFoundException Create<TEntity>(Expression<Func<TEntity, object>> expression, object key)
+            where TEntity : class
+        {
+            var member = (MemberExpression)expression.Body;
+            return new NotFoundException(typeof(TEntity).Name, member.Member.Name, key);
         }
     }
 }
