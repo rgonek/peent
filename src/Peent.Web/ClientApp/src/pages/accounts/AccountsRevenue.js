5d5271752ff416ec08dc3debab4466142d2d6f5c
Robert Gonek <rgonek@gmail.com>
2019-12-30 20:49:16 +0100

    Accounts revenue CRUD

---
+++ src/Peent.Web/ClientApp/src/pages/accounts/AccountsRevenue.js
@@ -0,0 +1,46 @@
+import React, { useMemo, useCallback } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
+import Accounts from './Accounts'
+
+function AccountsRevenue(props) {
+  const fetchData = useCallback((pageIndex, pageSize, sortBy, filters) => {
+    const filtersExpense = {
+      ...filters,
+      type: [4]
+    }
+    props.onFetchAccounts(pageIndex, pageSize, sortBy, filtersExpense);
+  }, []);
+
+  return (
+    <Accounts 
+      title='Accounts revenue'
+      url='/accounts/revenue/'
+      loading={props.loading}
+      pageCount={props.pageCount}
+      rowCount={props.rowCount}
+      accounts={props.accounts}
+      fetchData={fetchData} />
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    accounts: state.account.accounts,
+    loading: state.account.loading,
+    pageCount: state.account.pageCount,
+    rowCount: state.account.rowCount
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchAccounts: (pageIndex, pageSize, sortBy, filters) =>
+      dispatch(actions.fetchAccounts(pageIndex, pageSize, sortBy, filters))
+  };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(AccountsRevenue);
