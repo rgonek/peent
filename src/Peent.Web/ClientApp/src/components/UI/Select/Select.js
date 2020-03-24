9cd59f720704a156952dcb204215f685ad9f264a
Robert Gonek <rgonek@gmail.com>
2020-03-24 22:04:58 +0100

    Add select component

---
+++ src/Peent.Web/ClientApp/src/components/UI/Select/Select.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import ReactSelect from "react-select";
+import "./Select.css";
+
+function Select({
+  control,
+  name,
+  isInvalid,
+  defaultValue,
+  onChange,
+  isMulti = false,
+  className,
+  isClearable = true,
+  isSearchable = true,
+  ...restProps
+}) {
+  const {
+    setValue,
+    register,
+    mode: { isOnSubmit, isOnBlur, isOnChange },
+    reValidateMode: { isReValidateOnBlur, isReValidateOnSubmit }
+  } = control;
+
+  const [stateValue, setStateValue] = useState(defaultValue);
+
+  useEffect(() => {
+    register({ name: name });
+  }, []);
+
+  const handleChange = (value, action) => {
+    const val = isMulti ? value?.map(v => v.value) ?? [] : value?.value;
+    setValue(name, val, true);
+    setStateValue(val);
+    if (onChange) onChange(value, action);
+  };
+
+  const handleBlur = (e) => {
+    const shouldReValidateOnBlur = isOnBlur || isReValidateOnBlur;
+    if(shouldReValidateOnBlur)
+      setValue(name, stateValue, true);
+  };
+
+  const classNames = ["react-select", isInvalid && "is-invalid", className].join(" ");
+
+  return (
+    <ReactSelect
+      name={name}
+      isMulti={isMulti}
+      isClearable={isClearable}
+      isSearchable={isSearchable}
+      onChange={handleChange}
+      className={classNames}
+      onBlur={handleBlur}
+      {...restProps}
+    />
+  );
+}
+
+export default Select;
